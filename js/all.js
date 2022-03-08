85658db7169d911a3a5764ead5fe529bc7fcae72
Vic <syuan0212@gmail.com>
2022-03-08 11:16:27 +0800

    修正 all.js ESLint

--- js/all.js
+++ js/all.js
@@ -1,15 +1,16 @@
-const url    = 'https://hexschool.github.io/js-filter-data/data.json';
-const table  = document.querySelector('.table-content');
+const axios = require('axios');
+
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
+const table = document.querySelector('.table-content');
 const filter = document.querySelector('.filter');
 let data;
 let showData = [];
 let category = '';
 
-function renderData(inputData){
+function renderData(inputData) {
   let str = '';
   inputData.forEach((item) => {
-    let content =
-      `<tr>
+    const content = `<tr>
         <td> ${item['作物名稱']} </td> 
         <td> ${item['市場名稱']} </td>
         <td> ${item['上價']} </td>
@@ -17,26 +18,24 @@ function renderData(inputData){
         <td> ${item['中價']} </td>
         <td> ${item['平均價']} </td>
         <td> ${item['交易量']} </td>
-      </tr>`
+      </tr>`;
     str += content;
-  })
+  });
   table.innerHTML = str;
 }
 
-function filterCategory(event){
-  if(event.target.nodeName == 'BUTTON') {
+function filterCategory(event) {
+  if (event.target.nodeName === 'BUTTON') {
     category = event.target.dataset.category;
     showData = data.filter((item) => item['種類代碼'] === category);
     renderData(showData);
-  } else {
-    return;
   }
 }
 
 axios.get(url)
- .then((res) => {
-  data = res.data.filter((item) => item['作物名稱']);
-  renderData(data);
-})
+  .then((res) => {
+    data = res.data.filter((item) => item['作物名稱']);
+    renderData(data);
+  });
 
 filter.addEventListener('click', filterCategory);
